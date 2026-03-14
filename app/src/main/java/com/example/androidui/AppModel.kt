86e8d80c9250ef1014fa3e1bd884c6d54f83e5f5
Dc0der_1 <Dc0der_1@gmail.com>
2026-03-14 09:58:35 +0300

    Add MVVM: AppListViewModel, Snackbar on logo click

--- app/src/main/java/com/example/androidui/AppModel.kt
+++ app/src/main/java/com/example/androidui/AppModel.kt
@@ -7,12 +7,3 @@ data class AppModel(
     val category: String,
     val iconRes: Int
 )
-
-val appsList = listOf(
-    AppModel(1, "СберБанк Онлайн", "Больше чем банк", "Финансы", android.R.drawable.ic_menu_myplaces),
-    AppModel(2, "Яндекс.Браузер", "Быстрый и безопасный браузер", "Инструменты", android.R.drawable.ic_menu_search),
-    AppModel(3, "Почта Mail.ru", "Почтовый клиент для любых ящиков", "Инструменты", android.R.drawable.ic_dialog_email),
-    AppModel(4, "Яндекс Навигатор", "Парковки и заправки — по пути", "Транспорт", android.R.drawable.ic_menu_compass),
-    AppModel(5, "Мой МТС", "Мой МТС — центр экосистемы МТС", "Инструменты", android.R.drawable.ic_menu_call),
-    AppModel(6, "Яндекс — с Алисой", "Яндекс — поиск всегда под рукой", "Инструменты", android.R.drawable.ic_menu_agenda),
-)
